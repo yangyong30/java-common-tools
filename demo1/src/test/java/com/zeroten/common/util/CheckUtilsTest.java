a5f86d75a828aad6e5a83a1049ce0a6154620929
fengjue <553492051@qq.com>
2019-12-12 23:25:06 +0800

    初始化项目

---
+++ demo1/src/test/java/com/zeroten/common/util/CheckUtilsTest.java
@@ -0,0 +1,12 @@
+package com.zeroten.common.util;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckUtilsTest {
+    @Test
+    public void isAnyEmptyTest(){
+
+    }
+}
