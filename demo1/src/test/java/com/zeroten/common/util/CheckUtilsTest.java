09e2fc1e32557ac2138e018e3a8bbe6094e258f7
fengjue <553492051@qq.com>
2019-12-15 22:36:17 +0800

    “测试Array.asList()将int[]作为参数

--- demo1/src/test/java/com/zeroten/common/util/CheckUtilsTest.java
+++ demo1/src/test/java/com/zeroten/common/util/CheckUtilsTest.java
@@ -6,6 +6,8 @@ import org.testng.annotations.Test;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class CheckUtilsTest {
     @Test
@@ -17,5 +19,20 @@ public class CheckUtilsTest {
     public void testSort() {
         int[] arr = {1, 23, 5, 6, 1, 2, 3};
         Assert.assertEquals(ArrayUtils.sort(arr), new int[]{1, 1, 2, 3, 5, 6, 23});
+    }
+
+    @Test
+    public void testQQPage() {
+        Integer[] aa = {1, 2, 3, 55, 99};
+        int[] bb = {22, 33, 44, 55, 66};
+        int[] bb1 = {22, 33, 44, 55, 66};
+        List<Integer> integers = Arrays.asList(aa);
+        List<int[]> ints = Arrays.asList(bb,bb1);
+
+        System.out.println(integers + ":" + integers.size());
+        System.out.println(ints + ":" + ints.size());
+        for (int i = 0; i < ints.size(); i++) {
+            System.out.println(Arrays.toString(ints.get(i)));
+        }
     }
 }
