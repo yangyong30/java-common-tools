e9b612c33fd6369fad8dba53bd97221e39a23684
fengjue <553492051@qq.com>
2019-12-15 22:02:13 +0800

    数组的排序方法

---
+++ demo1/src/main/java/com/zeroten/common/util/ArrayUtils.java
@@ -0,0 +1,10 @@
+package com.zeroten.common.util;
+
+import java.util.Arrays;
+
+public class ArrayUtils {
+    public static int[] sort(int[] arr) {
+        Arrays.sort(arr);
+        return arr;
+    }
+}
--- demo1/src/test/java/com/zeroten/common/util/CheckUtilsTest.java
+++ demo1/src/test/java/com/zeroten/common/util/CheckUtilsTest.java
@@ -4,9 +4,18 @@ package com.zeroten.common.util;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class CheckUtilsTest {
     @Test
     public void test() {
         Assert.assertFalse(CheckUtils.isAnyEmpty("2", "", "2", null));
+    }
+
+    @Test
+    public void testSort() {
+        int[] arr = {1, 23, 5, 6, 1, 2, 3};
+        Assert.assertEquals(ArrayUtils.sort(arr), new int[]{1, 1, 2, 3, 5, 6, 23});
     }
 }
